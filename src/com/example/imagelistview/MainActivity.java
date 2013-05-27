dbd7fff2d29a46f388c163ec512eeea5c0144736
zhuang0 <sheen.zhuang@gmail.com>
2013-05-27 12:10:02 +0800

    根据Androi-universal-image-loader的Demo写的的ImageListView

---
+++ src/com/example/imagelistview/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.imagelistview;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.ImageLoadingListener;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.core.assist.SimpleImageLoadingListener;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
+
+public class MainActivity extends AbsListViewBaseActivity {
+
+	DisplayImageOptions options;
+	String[] imageUrls;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		imageUrls = Constants.IMAGES;
+
+		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
+				getApplicationContext()).threadPriority(Thread.NORM_PRIORITY - 2)
+				.denyCacheImageMultipleSizesInMemory()
+				.discCacheFileNameGenerator(new Md5FileNameGenerator())
+				.tasksProcessingOrder(QueueProcessingType.LIFO).enableLogging() // Not
+																				// necessary
+																				// in
+																				// common
+				.build();
+		// Initialize ImageLoader with configuration.
+		imageLoader.init(config);
+
+		options = new DisplayImageOptions.Builder().showStubImage(R.drawable.ic_stub)
+				.showImageForEmptyUri(R.drawable.ic_empty).showImageOnFail(R.drawable.ic_error)
+				.cacheInMemory().cacheOnDisc().displayer(new RoundedBitmapDisplayer(20)).build();
+
+		listView = (ListView) findViewById(R.id.listView1);
+		((ListView) listView).setAdapter(new ItemAdapter());
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onBackPressed() {
+		AnimateFirstDisplayListener.displayedImages.clear();
+		super.onBackPressed();
+	}
+
+	class ItemAdapter extends BaseAdapter {
+
+		private ImageLoadingListener animateFirstListener = new AnimateFirstDisplayListener();
+
+		private class ViewHolder {
+			public TextView text;
+			public ImageView image;
+		}
+
+		@Override
+		public int getCount() {
+			return imageUrls.length;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return position;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(final int position, View convertView, ViewGroup parent) {
+			View view = convertView;
+			final ViewHolder holder;
+			if (convertView == null) {
+				view = getLayoutInflater().inflate(R.layout.list_item, parent, false);
+				holder = new ViewHolder();
+				holder.text = (TextView) view.findViewById(R.id.text);
+				holder.image = (ImageView) view.findViewById(R.id.image);
+				view.setTag(holder);
+			} else {
+				holder = (ViewHolder) view.getTag();
+			}
+
+			holder.text.setText("Item " + (position + 1));
+
+			imageLoader.displayImage(imageUrls[position], holder.image, options,
+					animateFirstListener);
+
+			return view;
+		}
+	}
+
+	private static class AnimateFirstDisplayListener extends SimpleImageLoadingListener {
+
+		static final List<String> displayedImages = Collections
+				.synchronizedList(new LinkedList<String>());
+
+		@Override
+		public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+			if (loadedImage != null) {
+				ImageView imageView = (ImageView) view;
+				boolean firstDisplay = !displayedImages.contains(imageUri);
+				if (firstDisplay) {
+					FadeInBitmapDisplayer.animate(imageView, 500);
+					displayedImages.add(imageUri);
+				}
+			}
+		}
+	}
+
+}
